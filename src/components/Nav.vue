ac9558ab9398b15956fbfaffc637a3ae032e5fc8
mariovida <60817192+mariovida@users.noreply.github.com>
2022-01-12 15:00:45 +0100

    Create Nav.vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <Toggle :mode="mode" @toggle="$emit('toggle')" />
+  </nav>
+</template>
+
+<script>
+import Toggle from "@/components/Toggle";
+export default {
+  props: ["mode"],
+  components: {
+    Toggle,
+  },
+};
+</script>
+
+<style>
+#home.dark {
+  background-color: #363636;
+  color: #e8e8e8;
+}
+#home.dark h1 {
+  color: #839980;
+}
+</style>
