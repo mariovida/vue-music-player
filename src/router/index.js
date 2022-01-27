f83fcec5d5f68069cd2a814530604254ce8f7453
Mario Vidović <60817192+mariovida@users.noreply.github.com>
2022-01-27 19:17:30 +0100

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Songs from "../views/About.vue";
+import Contact from "../views/Contact.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/songs",
     name: "Songs",
     component: Songs,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
   },
 ];
 
